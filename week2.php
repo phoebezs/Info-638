7d0ff243de892a51b03e7d0d1dea11e363f64ef6
Phoebe Stein <pzstein@gmail.com>
2019-02-06 17:55:42 -0500

    adding new files

---
+++ week2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<head>
+    <title>Week 2 Page</title>
+</head>
+<body>
+<?php
+#Constant example
+    define("MYWEATHER", "Terrible");
+    print MYWEATHER;
+    
+
+    
+# Example vars
+    $temp = 32;
+    $condition = "Flurries";
+    $newscast_text = "They said \"it will snow\"";
+    /*\ indicates to php to output that character, or escape character*/
+    echo "<h1>Hello, world!</h1>";
+    print "<p>Welcome to my Week 2 page.</p>";
+    print "<p>The news today said - $newscast_text</p>";
+    print "The train costs \$something";
+?>
+    <p>ONE</p>
+    <p>TWO</p>
+<?php
+    $isitniceout = false;
+    $hasvalue = NULL;
+    $isitniceout = (int)2.5;
+    $historic_temps = array(32,45,23,37.9);
+    print $historic_temps[1];
+?>    
+    <p>THREE</p>
+<?php
+    $first = "Phoebe";
+    $middle = "Zack";
+    $last = "Stein";
+    $age = 35;
+    print "$last, $first $middle ($age)";
+    print "<ul>\n <li>$last, $first $middle ($age)</li>
+    </ul>";
+    $myname = array($first, $middle, $last);
+    print (int) $first
+    
+?>    
+  <p>FOUR</p>  
+<?php
+   #Arithmetic operators
+    print 4/2;
+    print 4%2;
+    
+    $mynum = 11;
+    $mynum = $mynum + 1;
+    ++$mynum;
+    print $mynum;
+    
+    $year = 2019
+    /*#$year = $year + 5; 
+    #$year += 5;
+    $issunny = true;
+    
+    if (($year == 2016) && ($issuny == true)) {
+        print "<p>It's 2019!</p>";
+    }
+    */
+    
+?>
+    <p>FIVE</p>
+    
+<?php
+  
+ $fullname = $first . " " . $middle . " " . $last;
+    print $fullname;
+    
+    
+print $age += 5;
+
+    $difference = (2034-2019) +$age;
+    print $difference;
+       
+    
+?>    
+    
+</body>
+</html>
---
+++ week3.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <ttile>Hello again!</title>
+<style>
+table, td {
+    border: 2px solid black;
+    border-collapse: collapse;
+}
+.squares {
+    background-color: yellow;
+}
+</style>
+</head>
+<body>
+<?php
+$avalue = 10;
+echo ++$avalue;
+echo ($avalue+10);
+
+$weathercondition = "Sunny";
+$temp = 45;
+if ($weathercondition == "Sunny") {
+    $pack_umbrella = False;
+
+}
+elseif ($temp <50) {
+    $pack_fleece = True;
+}
+
+else {
+    $pack_umbrella = True;
+}
+
+switch ($temp) {
+    case 45:
+        echo "It's cold!";
+        break;
+    case 55:
+        echo "It's warm!";
+        break;
+    default:
+        echo "It is $temp";
+        break;
+}
+?>
+
+<?php
+#SPEED TRAP
+$my_speed = 82;
+$curr_date = date ("md");
+
+# check if it's bday
+if ($curr_date == "0205") {
+    $is_bday = True;
+    $my_speed -=5;
+} 
+
+if ($my_speed > 80) {
+    #big ticket
+    echo "<p>Big ticket!</p>";
+}
+elseif ($my_speed < 61) {
+    #no ticket
+    echo "<p>No ticket!</p>";
+}
+else {
+    #med ticket
+    echo "<p>Medium ticket!</p>";                 
+}
+?>
+
+<?php
+#LOOPS
+$my_speed = 82;
+
+while ($my_speed > 80) {
+   echo "<p>Slow down!</p>";
+    --$my_speed;
+    echo "my speed is $my_speed";
+}
+do {
+    echo "<p>Slow down!</p>";
+    --$my_speed;
+    echo "my speed is $my_speed";
+
+} while ($my_speed > 80);
+
+#for ($i = 1; $i < 30; ++$i) {
+#    echo $i;
+#    break;
+#}
+?>
+
+<?php
+#TIMES TABLE
+
+echo "<table>";
+#repeat 12 times for each row
+
+for ($row = 1; $row <=12; ++$row){
+    echo "<tr>";
+}
+    #repeat 12 times for each column
+    for ($col = 1; $col <= 12; ++$col) {
+        if ($row==$col) {
+            echo "<td class=\"squares\">";
+        } else {
+            echo "<td>";
+        }
+    echo "<td>" . ($row*$col) . "</td>";
+    #repeat 12 times for each cell/column  
+    }
+
+?>
+
+</body>
+</html>
