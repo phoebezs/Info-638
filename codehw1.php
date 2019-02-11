bb6bad104b3555b54627f803cf13337e2df94ebe
Phoebe Stein <pzstein@gmail.com>
2019-02-10 22:07:43 -0500

    Update codehw1.php

--- codehw1.php
+++ codehw1.php
@@ -10,11 +10,38 @@
 </head>
 
 <body>
+<h1>Challenge: Correct Change</h1>
 <?php
+$mychange=159;
+$coins = array (100, 25, 10, 5, 1);
+$dollar=100;
+$quarter=25;
+$dime=10;
+$nickel=5;
+$penny=1;
 
+echo "<p>You are due $mychange cents back in total.</p>";
+print (int) $mychange%100;
 
+?>
 
+<h1>Challenge: 5 Bottles of Beer</h1>
 
+<?php
+$beer=5;
+
+/*for ($beer=5; $beer>=0; --$beer) 
+{
+    echo "$beer bottles of beer on the wall, $beer bottles of beer! Take one down, pass it around, " . --$beer . " bottles of beer on the wall!"; 
+    echo "<br>";
+
+}*/
+
+do 
+{
+    echo "<p>$beer bottles of beer on the wall, $beer bottles of beer! Take one down, pass it around, " . --$beer . " bottles of beer on the wall!</p>"; 
+    echo "<br>";
+} while ($beer >=1);
 
 ?>
 </body>
