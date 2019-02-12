fa6de306cce1ddda1bbb23913c64fa63d7d8f4f7
Phoebe Stein <pzstein@gmail.com>
2019-02-12 17:21:23 -0500

    Update codehw1.php

--- codehw1.php
+++ codehw1.php
@@ -5,42 +5,51 @@
 </title>
 
 <style>
+h2 {
+    font-family: helvetica; 
+}
 
+p {
+    font-family: helvetica; color: green;
+}
 </style>
 </head>
 
 <body>
-<h1>Challenge: Correct Change</h1>
+<h2>Challenge: Correct Change</h2>
 <?php
 $mychange=159;
-$coins = array (100, 25, 10, 5, 1);
-$dollar=100;
-$quarter=25;
-$dime=10;
-$nickel=5;
-$penny=1;
+
+
+$dollars = (int) ($mychange/100); // 1 dollar
+$remainder1 = $mychange%100; // 59 cents left over
+
+$quarters = (int) ($remainder1/25); // 2 quarters
+$remainder2  = $remainder1%25; // 9 cents left over
+
+$dimes = (int) ($remainder2/10); // 0 dimes
+
+$nickels = (int) ($remainder2/5); // 1 nickel
+$remainder3 = $remainder2%5; // 4 cents left over
+
+$pennies = $remainder3;
+
 
 echo "<p>You are due $mychange cents back in total.</p>";
-print (int) $mychange%100;
+print "<p>You are due back $dollars dollar, $quarters quarters, $dimes dimes, $nickels nickel, and $pennies pennies.</p><br>";
 
 ?>
 
-<h1>Challenge: 5 Bottles of Beer</h1>
+
+<h2>Challenge: 5 Bottles of Beer</h2>
 
 <?php
 $beer=5;
 
-/*for ($beer=5; $beer>=0; --$beer) 
-{
-    echo "$beer bottles of beer on the wall, $beer bottles of beer! Take one down, pass it around, " . --$beer . " bottles of beer on the wall!"; 
-    echo "<br>";
-
-}*/
-
+#Subtracts 1 from $beer at the end of each loop
 do 
 {
     echo "<p>$beer bottles of beer on the wall, $beer bottles of beer! Take one down, pass it around, " . --$beer . " bottles of beer on the wall!</p>"; 
-    echo "<br>";
 } while ($beer >=1);
 
 ?>
