8286633a2e6be17ee04b1772509173ce966c88ec
Phoebe Stein <pzstein@gmail.com>
2019-04-07 21:31:53 -0400

    deleted week 10 files

--- week-10/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'login.php';
-$conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die($conn->connect_error);
-$query = "INSERT INTO volunteers VALUES (NULL, 'Sam', 'Samson', '2000-09-29')";
-$result = $conn->query($query);
-if (!$result) die ("Database access failed: " . $conn->error);
-echo "The Insert ID was: " . $conn->insert_id;
-?>
