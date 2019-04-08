dff85689d9d5b3b67830b3fe5a8d7455254af6e1
phoebezs <47196273+phoebezs@users.noreply.github.com>
2019-04-07 21:28:47 -0400

    Delete drop-table.php

--- week-10/drop-table.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'login.php';
-$conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die($conn->connect_error);
-$query = "DROP TABLE volunteers";
-$result = $conn->query($query);
-if (!$result) die ("Database access failed: " . $conn->error);
-?>
